3d23153966d9c32f78d83983228030bdeb1cf4b3
Harold Kim <harold.kim@snowflake.com>
2025-03-28 16:27:24 -0700

    Changed home description

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
     <ProjectPageTemplate>
       <PageHeader
         title="DesignLab"
-        description={<>Welcome to <a href="https://snowflake.enterprise.slack.com/team/U088AC6FKEH" target="_blank" rel="noopener noreferrer" className="hover:text-white">@harold.kim</a>&apos;s personal sandbox at Snowflake Design.<br />All concepts are built in Next.js, Tailwind, and Balto. Please do not reference this document as a source of truth.</>}
+        description={<>Welcome to the Snowflake Design System testing grounds at Snowflake.<br />All concepts are built in Next.js, Tailwind, and Balto. Please do not reference this document as a source of truth.</>}
         centered
       />
     </ProjectPageTemplate>
